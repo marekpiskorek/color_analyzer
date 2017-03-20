63dd6ce19ad3456c0fcc0f7260e13e6d43a68405
Marek Piskorek <marek.piskorek@gmail.com>
2017-03-20 20:45:27 +0100

    Fix script name in test.

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ import subprocess
 
 
 def test_empty_dir():
-    result = subprocess.check_output(['python', 'interpreter.py'])
+    result = subprocess.check_output(['python', 'analyzer.py'])
     assert result == '>>> There are no image files!\n'
 
 
 def test_two_images():
-    result = subprocess.check_output(['python', 'interpreter.py', 'data/'])
+    result = subprocess.check_output(['python', 'analyzer.py', 'data/'])
     assert result.split('\n') == [
         '>>> data/magical.png:', '> red', '> black', '> white',
         '>>> data/all-the-sweaters.jpg:', '> white', '> black', '> red', ''
